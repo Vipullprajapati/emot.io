e7dfb14dec78eeb8c7103d2fea2be44d5af30ef8
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-02-02 11:33:33 +0000

    create Emote.tsx

---
+++ client/app/components/Emote.tsx
@@ -0,0 +1,31 @@
+import { Card } from "@heroui/card";
+import { Button } from "@heroui/button";
+import { useState } from "react";
+
+function Emote() {
+  const [emoji, setEmoji] = useState("😏");
+
+  return (
+    <main className="min-h-screen flex items-center justify-center gap-4 flex-col">
+      <h1 className="text-3xl lowercase font-bold">emote.io</h1>
+      <Card className="text-6xl p-3">{emoji}</Card>
+      <ShowEmotes setEmoji={setEmoji} />
+    </main>
+  );
+}
+
+function ShowEmotes({ setEmoji }) {
+  const emojis = ["😀", "😁", "😂", "😎", "🙄"];
+
+  return (
+    <div className="flex gap-2 flex-wrap justify-center">
+      {emojis.map((emoji) => (
+        <Button key={emoji} variant="flat" onPress={() => setEmoji(emoji)}>
+          <p className="text-xl">{emoji}</p>
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+export default Emote;
